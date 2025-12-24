f28d46ea43d6467d478fc9951343e7ece2e10168
Vijay Kumar <94276052+vkumar-31@users.noreply.github.com>
2025-12-24 19:13:16 +0530

    docs: fix typo in the class name

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -6,7 +6,7 @@
  * * Time Complexity: $O(n^2)$ - We visit each element in both arrays once.
  * Space Complexity: $O(1)$ - We modify the input array in-place.
  */
-class Solution {
+class TwoSum {
      public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
             for(int j = 0; j < nums.length; j++){
