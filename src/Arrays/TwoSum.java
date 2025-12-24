8a38a2b87251ee01398c4d93aa373c37fd565da2
Vijay Kumar <94276052+vkumar-31@users.noreply.github.com>
2025-12-24 18:44:48 +0530

    feat: add two sum Array solution

---
+++ src/Arrays/TwoSum.java
@@ -0,0 +1,20 @@
+/**
+ * Problem: Two Sum  (LeetCode #001)
+ * Pattern: Two Pointers (Forwards)
+ * * Logic: Since the first array has extra space at the end, we start 
+ * comparing elements from the back to avoid overwriting values.
+ * * Time Complexity: $O(n^2)$ - We visit each element in both arrays once.
+ * Space Complexity: $O(1)$ - We modify the input array in-place.
+ */
+class Solution {
+     public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(i != j && nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
