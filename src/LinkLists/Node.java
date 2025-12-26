397f0b3d358de9b009d0e54d664ff3bd04890841
Vijay Kumar <94276052+vkumar-31@users.noreply.github.com>
2025-12-26 18:43:08 +0530

    feat: add Node class definition for Singly Linked List

---
+++ src/LinkLists/Node.java
@@ -0,0 +1,24 @@
+/**
+ * DATA STRUCTURE: Node (Singly Linked List)
+ * -----------------------------------------
+ * DESCRIPTION:
+ * The building block of a Singly Linked List. Contains data and 
+ * a reference to the next node in the sequence.
+ *
+ * COMPLEXITY:
+ * - Space: O(1) per node.
+ * - Access: O(1) for data/next pointer.
+ */
+class Node{
+    private int data;
+    private  Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
