f530f74b87eba74849d76d4de05dd644ab5986c9
Vijay Kumar <94276052+vkumar-31@users.noreply.github.com>
2025-12-28 19:47:42 +0530

    refactor : capitalize the letter of Leetcode

--- src/LinkLists/LinkListCycle.java
+++ src/LinkLists/LinkListCycle.java
@@ -1,5 +1,5 @@
 /**
- * LeetCode 141: Floyd's Cycle-Finding (Tortoise and Hare)
+ * LEETCODE 141: Floyd's Cycle-Finding (Tortoise and Hare)
  * ------------------------------------------------------
  * PROBLEM: Given the head of a linked list, determine if the list has a cycle(Leetcode 141).
  * * LOGIC:
