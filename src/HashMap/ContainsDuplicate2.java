1b07307938f6056f44fef31689ca269284bad2c7
VijayKumarCode <94276052+VijayKumarCode@users.noreply.github.com>
2026-01-09 18:50:18 +0530

    refactor: add inline header and fix code formatting
        Move containing directory(HashMap) to top of problem header
    
        Fixed minor typos and improved spacing for better readability

--- src/HashMap/ContainsDuplicate2.java
+++ src/HashMap/ContainsDuplicate2.java
@@ -1,4 +1,5 @@
 package HashMap;
+import java.util.HashSet;
 /*
  * Problem: 219. Contains Duplicate II
  * Difficulty: Easy
@@ -6,25 +7,28 @@ package HashMap;
  * Time Complexity: O(n)
  * Space Complexity: O(min(n, k))
  */
-import java.util.HashSet;
-class ContainDuplicate2 {
+
+class ContainsDuplicate2 {
 private static boolean containDuplicate(int[] nums, int k) {
     // A HashSet to store elements in the current window of size k
     HashSet<Integer> set = new HashSet<>();
+
     for (int i = 0; i < nums.length; i++) {
         // If the current element is already in the set,
         // it means we found a duplicate within distance k
         if (set.contains(nums[i])) {
             return true;
         }
+
         // Add current element to the set
         set.add(nums[i]);
+
         // If the set size exceeds k, remove the oldest element (at index i - k)
         // This maintains the "sliding window" of size k
-        if (set.size() > k) {
+            if (set.size() > k) {
             set.remove(nums[i - k]);
-        }
-      }
-      return false;
+           }
+       }
+        return false;
    }
 }
