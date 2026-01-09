14196ec8a660e708bf9160d3922c584c6d6de428
Vijay Kumar <94276052+VijayKumarCode@users.noreply.github.com>
2026-01-09 19:04:09 +0530

    refactor : inline the code and enhance the readability

--- src/Array/IntToRoman.java
+++ src/Array/IntToRoman.java
@@ -1,8 +1,9 @@
 package Array;
+
 /**
- * 🔢 LEETCODE 12: Integer to Roman
- * --------------------------------
+ * Problem No.#12: Integer to Roman
  * PROBLEM: Convert an integer to a Roman numeral.
+ * Difficulty : Easy
  * * ALGORITHM STRATEGY: Greedy Algorithm
  * 1. Mapping: Create an array of values and corresponding Roman symbols 
  * including subtractive cases (CM, CD, XC, XL, IX, IV).
@@ -14,11 +15,15 @@ package Array;
  * symbols is fixed (max 13), and the input range is limited to 3999.
  * - Space Complexity: O(1) -> We use a constant amount of space for the mapping.
  */
+
 class IntToRoman {
+    
     private static String intToRoman(int number) {
+        
         StringBuilder sb = new StringBuilder();
         int[] values = {1000,900,500,400,100,90,50,40,10,5,1};
         String[] values2 = {"M","CM","D","CD","C","XC","L","XL","X","V","I"};
+        
         for (int i = 0; i < values.length; i++) {
             while (number >= values[i]) {
                     number -= values[i];
