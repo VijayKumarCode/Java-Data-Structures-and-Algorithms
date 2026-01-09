d6e77f6db25bc649f5ec885821df14dc52e7a641
Vijay Kumar <94276052+VijayKumarCode@users.noreply.github.com>
2026-01-09 19:27:24 +0530

    Update RemoveDuplicates.java

--- src/Array/RemoveDuplicates.java
+++ src/Array/RemoveDuplicates.java
@@ -1,24 +1,32 @@
 package Array;
+
 /**
- * Problem: Remove Duplicates (LeetCode #080)
+ * Problem No.#080
+ * Difficulty : Medium
+ * Description : Remove Duplicates 
  * Pattern: Two Pointers (Read/Write)
  * * Logic: comparing adjacent two pointers simaltaneously
  * * inorder to eliminate thrice appearence of any number in the array
  * * Time Complexity: $O(N)$ - Single pass through the array.
  * Space Complexity: $O(1)$ - No extra space used.
  */
-class RemoveDuplicates {
+
+class RemoveDuplicates2 {
+    
     public int removeDuplicates(int[] nums) {
         int currentIndex = 1;
         int counter = 0;
         int prevValue = nums[0];
+        
         for(int i = 1; i < nums.length; i++) {
+            
             if(nums[i] != prevValue) {
                 counter = 0;
             }
             else {
                 counter++;
             }
+            
             if(counter <= 1) {
                 nums[currentIndex++] = nums[i];
                 prevValue = nums[i];
