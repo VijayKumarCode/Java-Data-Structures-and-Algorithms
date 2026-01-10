5b4348994d4f2c70190a26f6829c4406d095a885
VijayKumarCode <94276052+VijayKumarCode@users.noreply.github.com>
2026-01-10 16:48:12 +0530

    refactor : inline the code and header for better readability

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -1,21 +1,28 @@
 package Array;
+
 /**
- * Problem: Two Sum  (LeetCode #001)
+ * Problem No.#001
+ * Difficulty : Easy
+ * Description : Two sum
  * Pattern: Two Pointers (Forwards)
  * * Logic: We have to take two pointer approach and make sure none of them call 
  * itself as it violates the question theme
  * * Time Complexity: $O(n^2)$ - We visit each element in both arrays once.
  * Space Complexity: $O(1)$ - We modify the input array in-place.
  */
+
 class TwoSum {
+
      public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                if(i != j && nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
-        }
+         for(int i = 0; i < nums.length; i++) {
+
+             for(int j = 0; j < nums.length; j++) {
+
+                 if(i != j && nums[i] + nums[j] == target) {
+                     return new int[]{i,j};
+                 }
+              }
+          }
         return new int[]{-1,-1};
     }
 }
